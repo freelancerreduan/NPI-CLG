319658b07231fca8f4645b4a88b53f2e031580c6
freelancerreduan <mdreduan0005@gmail.com>
2023-08-12 11:16:22 +0600

    this update clg admin panel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,6 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-
-
-
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'phone' => ['required', 'string', 'max:255'],
@@ -69,11 +66,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-
-
-
-
-
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
